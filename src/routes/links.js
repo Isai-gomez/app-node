b41d78dfa0baf1e5f1fbfbb47579b275426eeb51
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-05 16:08:14 -0500

    Funciona el POST

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,7 +5,8 @@ router.get('/add',(req, res) => {
 res.render('links/add');
 });
 router.post('/add', (req, res) => {
-res.send('Recibido');
+ console.log(req.body);
+ res.send('Recibido');
 });
 module.exports = router;
 
