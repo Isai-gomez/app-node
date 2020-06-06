c506196dd44450451528011ed886f1d675ba859e
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-06 15:05:29 -0500

    Elimina de la DB

--- src/routes/links.js
+++ src/routes/links.js
@@ -12,12 +12,17 @@ const newLink = {
  description
 };
 await pool.query('INSERT INTO links set ?', [newLink]);
- res.send('Recibido');
+ res.redirect('/links');
 });
 router.get('/', async (req, res) => {
  const links = await pool.query('SELECT * FROM links');
  console.log(links)
  res.render('links/list', { links });
+});
+router.get('/delete/:id', async (req, res) => {
+	const { id } = req.params;
+ 	await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+ 	res.redirect('/links');
 });
 module.exports = router;
 
