4e05c0d69fa0061c71bb77aea96ed2dde4501f9b
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-06 17:39:03 -0500

    Vista de editar funciona

--- src/routes/links.js
+++ src/routes/links.js
@@ -16,13 +16,17 @@ await pool.query('INSERT INTO links set ?', [newLink]);
 });
 router.get('/', async (req, res) => {
  const links = await pool.query('SELECT * FROM links');
- console.log(links)
  res.render('links/list', { links });
 });
 router.get('/delete/:id', async (req, res) => {
 	const { id } = req.params;
  	await pool.query('DELETE FROM links WHERE ID = ?', [id]);
  	res.redirect('/links');
+});
+router.get('/edit/:id', async (req, res) => {
+	const { id } = req.params;
+ 	console.log(id);
+ 	res.send('RECEIVED');
 });
 module.exports = router;
 
