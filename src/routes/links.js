552ea90ef38ccf588807ef46025642fefeaf22c1
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-07 05:02:54 -0500

    Puedo editar links

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,33 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../database');
-router.get('/add',(req, res) => {
-res.render('links/add');
+const pool = require("../database");
+router.get("/add", (req, res) => {
+  res.render("links/add");
 });
-router.post('/add', async (req, res) => {
-const { title, url, description } = req.body;
-const newLink = {
- title,
- url, 
- description
-};
-await pool.query('INSERT INTO links set ?', [newLink]);
- res.redirect('/links');
+router.post("/add", async (req, res) => {
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  await pool.query("INSERT INTO links set ?", [newLink]);
+  res.redirect("/links");
 });
-router.get('/', async (req, res) => {
- const links = await pool.query('SELECT * FROM links');
- res.render('links/list', { links });
+router.get("/", async (req, res) => {
+  const links = await pool.query("SELECT * FROM links");
+  res.render("links/list", { links });
 });
-router.get('/delete/:id', async (req, res) => {
-	const { id } = req.params;
- 	await pool.query('DELETE FROM links WHERE ID = ?', [id]);
- 	res.redirect('/links');
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM links WHERE ID = ?", [id]);
+  res.redirect("/links");
 });
-router.get('/edit/:id', async (req, res) => {
-	const { id } = req.params;
- 	const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
-  	res.render('link/edit', { links:links[0] });
-
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const link = await pool.query("SELECT * FROM links WHERE id = ?", [id]);
+  res.render("links/edit", { link: link[0] });
+});
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, url, description } = req.body;
+  const newlinks = {
+    title,
+    url,
+    description,
+  };
+  await pool.query("UPDATE links set ? WHERE id = ?", [newlinks, id]);
+  res.redirect("/links");
 });
 module.exports = router;
-
