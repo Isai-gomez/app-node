1a81c77634c1169cc72fdfea63f368bb35f97dce
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-08 00:09:34 -0500

    Formulario listo

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
+router.get('/singup', (req, res) => {
+	res.render('auth/singup');
+});
+router.post('/singup', (req, res) => {
+
+});
 module.exports = router;
