5ae7330342042ed73907f46bedfdd0930fa86874
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-09 16:23:13 -0500

    puedo ir a la ruta profile

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,12 +1,28 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const pool = require("../database");
+const helpers = require('../lib/helper');
 passport.use("local.singup", new LocalStrategy(
  {
 	usernameField:'username',
   	passwordField:'password',
   	passReqToCallback: true,
  }, async (req, username, password, done) => {
-	console.log(req.body);
- }
-));
+  	const { fullname } = req.body;
+   	const newUser = {
+		username,
+	 	password,
+	 	fullname,
+ 	}
+  newUser.password = await helpers.encryptPassword(password);
+  const result = await pool.query('INSERT INTO users SET ?', [ newUser ]);
+  newUser.id = result.insertId;
+  return done(null, newUser);
+ }));
+passport.serializeUser((user, done) => {
+	done(null, user.id)
+});
+passport.deserializeUser(async (id, done) => {
+	const row = await pool.query('SELECT * FROM users Where id = ?',[id]);
+ 	done(null, row[0]);
+});
