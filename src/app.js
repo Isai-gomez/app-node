163d4e12d6911ee9706767fdf70863b30b639980
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-10 09:24:45 -0500

    Veo los message en la vista

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ app.use(passport.session());
 //Global Variabls
 app.use((req, res, next) => {
  app.locals.success = req.flash('success');
+ app.locals.success = req.flash('message');
 next();
 })
 //Router
--- src/lib/passport.js
+++ src/lib/passport.js
@@ -8,9 +8,18 @@ passport.use("local.signin", new LocalStrategy({
         passReqToCallback: true,
 
 }, async(req, username, password, done) => {
-	console.log(req.body);
- 	console.log(username);
- 	console.log(password);
+	const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+ 	if(rows.length > 0){
+		const user = rows[0];
+	 	const validPassword = await helpers.matchPassword(password, user.password);
+	 	if(validPassword){
+			done(null, user, req.flash("success","Welcome " + user.username));
+		}else{
+			done(null, false, req.flash("message","Incorrect Password"));
+		}
+	}else{
+		done(null, false, req.flash("message",'The username does not exists'));
+	}
 }));
 passport.use("local.singup", new LocalStrategy(
  {
